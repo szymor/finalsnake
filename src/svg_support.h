2872c2545c19fd6fe8855c2bf2c3390057eea2fe
a0w-svg <dawidkochanski86@gmail.com>
2022-11-11 14:10:27 -0500

    add svg support header file

---
+++ src/svg_support.h
@@ -0,0 +1,7 @@
+#ifndef _H_SVG_SUPPORT
+#define _H_SVG_SUPPORT
+#include <SDL/SDL.h>
+
+int IMG_isSVG(SDL_RWops* src);
+SDL_Surface* SVG_LoadSizedSVG_RW(const char* src, int width, int height);
+#endif 
